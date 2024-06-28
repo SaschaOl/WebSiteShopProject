ce9f56a11a4e69eff08ce7b2a86678da52090111
SaschaOl <127589730+SaschaOl@users.noreply.github.com>
2024-06-28 18:01:42 +0200

    Delete registration_page directory

--- registration_page/__init__.py
+++
@@ -1,2 +0,0 @@
-from .views import show_registration_page
-from .app import registration_page_app
--- registration_page/app.py
+++
@@ -1,8 +0,0 @@
-import flask
-
-registration_page_app = flask.Blueprint(
-    name = "registration_page",
-    import_name = "app",
-    template_folder = "registration_page/templates",
-    static_folder = "static"
-)
