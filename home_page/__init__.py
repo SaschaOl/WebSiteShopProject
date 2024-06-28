3e919fcf3765f9094f11b7024b914831b0ee2c7f
SaschaOl <127589730+SaschaOl@users.noreply.github.com>
2024-06-28 18:01:36 +0200

    Delete home_page directory

--- home_page/__init__.py
+++
@@ -1,2 +0,0 @@
-from .app import home_page_app
-from .views import show_home_page
--- home_page/app.py
+++
@@ -1,8 +0,0 @@
-import flask
-
-home_page_app = flask.Blueprint(
-    name = "home_app",
-    import_name = "app",
-    template_folder = "home_page/templates",
-    static_folder = "static"
-)
